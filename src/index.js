c15cacff65b563047e8367c6c97f72c40b92f3ee
tangsf <864126508@qq.com>
2017-04-28 10:11:09 +0800

    add eslingignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import "./static/lib/flexiblejs-0.3.4";
 
 ReactDOM.render(
   <App />,
